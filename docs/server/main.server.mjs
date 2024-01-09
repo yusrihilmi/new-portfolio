379f8f2d887d71e3d7f178a6722274e8da64e4c4
Yusri Hilmi <yusrihilmi4@gmail.com>
2024-01-09 15:15:53 +0700

    new update

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UUGJNW42.mjs";import"./chunk-ZTX7QZPO.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-L3JLS5XM.mjs";import"./chunk-BP72C5RC.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jsonData from './data/about.json';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  abouts: any[] = jsonData.abouts
 }
