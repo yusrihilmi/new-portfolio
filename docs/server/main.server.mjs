d24b642c54ddcd82d0e218c3fcdd5d3ef9a6459f
Yusri Hilmi <yusrihilmi4@gmail.com>
2024-01-18 17:51:34 +0700

    Adjusment Styling

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-D36RNDYO.mjs";import"./chunk-GHUDGI3Z.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-ORLW5CCN.mjs";import"./chunk-GHUDGI3Z.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
