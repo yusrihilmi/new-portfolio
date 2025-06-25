c274424a5bc3a32b538d15fc397ff76273766d3b
Yusri Hilmi <yusrihilmi4@gmail.com>
2025-06-25 15:47:16 +0700

    feat: new content in about for work experience

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YVTMKSTE.mjs";import"./chunk-KB3MCJ44.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IOUWOMAU.mjs";import"./chunk-KB3MCJ44.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
